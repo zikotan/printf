5ce923e3461d1887b9fe22a06081355acd584d07
zikotan <tananzakaria630@gmail.com>
2023-08-20 16:10:50 -0700

    response

--- main.h
+++ main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
 #define BUF_SIZE 1024
 
 /* the flags */
@@ -29,18 +31,52 @@ struct s_format
 	int (*fass)(va_list, char[], int, int, int, int);
 };
 
-
 /**
- * typedef struct s_format fmt_t - Struct op
+ * typedef struct s_format format_t - Struct op
  *
  * @s_format: The format.
  * @f_ass: The function associated.
  */
-typedef struct s_format fmt_t;
+typedef struct s_format format_t;
 
 int _printf(const char *format, ...);
 int handle_pr(const char *format, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+va_list list, char buf[], int fl, int wid, int prec, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int pr_char(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_string(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_percent(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+
+/* Functions to print numbers */
+int pr_int(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_binary(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_unsigned(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_octal(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_hex(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+int pr_hexa_upper(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+
+int pr_hexa(va_list types, char map_to[],
+char buf[], int fl, char flag_ch, int wid, int prec, int size);
+
+/* Function to print non printable characters */
+int pr_unprintable(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+
+/* Funcion to print memory address */
+int pr_pointer(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
 
 /* Funciotns to handle other specifiers */
 int get_fl(const char *format, int *i);
@@ -48,5 +84,33 @@ int get_wid(const char *format, int *i, va_list list);
 int get_prec(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-#endif
+/*Function to print string in reverse*/
+int pr_reverse(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
 
+/*Function to print a string in rot 13*/
+int pr_RotStr(va_list types, char buf[],
+	int fl, int wid, int prec, int size);
+
+/* wid handler */
+int handle_wr_char(char c, char buf[],
+	int fl, int wid, int prec, int size);
+int wr_number(int is_positive, int ind, char buf[],
+	int fl, int wid, int prec, int size);
+int wr_num(int ind, char bff[], int fl, int wid, int prec,
+	int length, char padd, char extra_c);
+int wr_pointer(char buf[], int ind, int length,
+	int wid, int fl, char padd, char extra_c, int padd_start);
+
+int wr_unsignd(int is_negative, int ind,
+char buf[],
+	int fl, int wid, int prec, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif
