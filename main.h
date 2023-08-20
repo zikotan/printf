1e32591c7625600ae4328a1718c5d12a285345a0
zikotan <tananzakaria630@gmail.com>
2023-08-20 12:23:59 -0700

    response

--- main.h
+++ main.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#define BUF_SZ 1024
 
 int _printf(const char *format, ...);
+int handling(const char *frmt, int *i, va_list list, char buf[], int fl, int w, int prec, int sz);
+
+int get_fl(const char *format, int *i);
+int get_w(const char *format, int *i, va_list list);
+int get_prec(const char *format, int *i, va_list list);
+int get_sz(const char *format, int *i);
 
 #endif
 
