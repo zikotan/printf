92c0e8100487c112d82cc40ea04b138e34304bbb
zikotan <tananzakaria630@gmail.com>
2023-08-22 13:35:52 -0700

    response

--- main.h
+++ main.h
@@ -110,6 +110,7 @@ char buffer[],
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
+void print_buffer(char buffer[], int *buff_ind);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
